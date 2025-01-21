be28f2db65cfbcdb3de84bdd82f6e12f2824f32d
Manas Arora <manasarora168@gmail.com>
2025-01-21 10:52:16 +0530

    ud

--- src/index.js
+++ src/index.js
@@ -63,7 +63,7 @@ export const myProjects = [
         subdesc:
             'Built as a unique Software-as-a-Service app with Next.js 14, Tailwind CSS, TypeScript, Framer Motion and Convex, Podcastr is designed for optimal performance and scalability.',
         href: 'https://www.youtube.com/watch?v=zfAb95tJvZQ',
-        texture: '/textures/project/sih.mov',
+        texture: '/textures/project/project1.mp4',
         logo: '/assets/project-logo1.png',
         logoStyle: {
             backgroundColor: '#2A1816',
